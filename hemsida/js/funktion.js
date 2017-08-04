5fda6251788bdaf897247a59f8db6acf45161b3f
Feliciasbooks <30719813+Feliciasbooks@users.noreply.github.com>
2017-08-04 10:56:43 +0200

    Add files via upload

---
+++ hemsida/js/funktion.js
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCzuwSYolCfADgoF_V9-J_2jG7bmYCrp0M",
+    authDomain: "bockerfelicia.firebaseapp.com",
+    databaseURL: "https://bockerfelicia.firebaseio.com",
+    projectId: "bockerfelicia",
+    storageBucket: "bockerfelicia.appspot.com",
+    messagingSenderId: "508307535727"
+  };
+  firebase.initializeApp(config);
+
+
+
+var app = angular.module("app", ["firebase"]);
+
+app.factory("kommentarer", function($firebaseArray) {
+var ref = firebase.database().ref().child("kommentarer");
+ return $firebaseArray(ref);
+  }
+);
+pp.controller("KommentarCtrl", function($scope, kommentarer) {
+     $scope.kommentarer = kommentarer;
+ 
+
+  }
+});
+
+
+app.controller("KommentarCtrl", function($scope, kommentarer) {
+    $scope.kommentarer = kommentarer;
+
+    
+    $scope.kommentar = {
+        text: "",
+        skribent: ""
+    };
+  
+  }$scope.addComment = function() {
+ $scope.kommentarer.$add($scope.kommentar);
+        $scope.kommentar = {
+        text: "",
+        skribent: ""
+    };
+;
+});
+
+
+
